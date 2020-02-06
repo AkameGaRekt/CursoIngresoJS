2929b923a18d9c588fe2045a873843cf465eb56c
Weiss Schnee <steamaxi250@gmail.com>
2020-02-06 11:13:48 -0300

    Tutorial Clase 1 (Ejercicio 1)
    Aprendiendo a programar con el Alert y esas cosas

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,21 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+/*Todo esto es solo
+para el mensaje
+del boton de la primera parte
+
+*/
 function mostrar()
 {
-	alert("ok");
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido="Nadie";
+
+	apellido="By Maxi";
+	//alert(apellido);
+	// Es un valor literal si esta
+	alert("Esto funciona de maravilla");//Muestra el titulo o mensaje de bienvenida
+	//alert("");//Literal
+    alert(apellido);//Muestra Al creador despues del titulo o mensaje de bienvenida
+    //alert("apellido");//Literal
+    alert("Change da world my final message goodbye");//Mensaje de despedida
 }
 
