3721851697166968f9ca56d74912851385ba9800
Weiss Schnee <steamaxi250@gmail.com>
2020-02-06 12:11:00 -0300

    tutorial clase 1 (paso 2)
    exitos

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,9 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var person = prompt("Nombre de Usuario", "");
+	var nombre 
+	console.log ("Ponga su nombre de Usuario " + person); 
+}
+
